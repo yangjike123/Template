f6c08ac07042370f7b10c2ee62c47dab902b3dd4
yanjike <2711396853@qq.com>
2022-09-06 19:25:27 +0800

    登入

--- viteAdminTs/src/models/Login.ts
+++ viteAdminTs/src/models/Login.ts
@@ -1,12 +1,15 @@
+import { message } from "antd";
+import { bindJWTToken, requestPost } from "../utils";
 import { namespace } from "../utils/enums"
-
-const ELogin = namespace("ELogin")
+import { Md5 } from 'ts-md5';
+import { ELocalStorage } from "../cofing/Enum";
+export const ELogin = namespace("ELogin")
 export default {
     namespace: ELogin.Name,
     state: {
-        examData: null,
-        editFlag: null,
-        data: null,
+        currentUser: null,
+        roleInfo: null,
+        status: ELocalStorage.Login
     },
     reducers: {
         [ELogin.RSetState](state: any, payload: any) {
@@ -17,8 +20,31 @@ export default {
         },
     },
     effects: {
-        // [ELogin.EPosLogin]({ state, payload }, { reducer, select, effect }) {
-
-        // }
+        async [ELogin.EPost]({ state, payload }: any, { reducer, select, effect }: any) {
+            payload.password = Md5.hashAsciiStr(payload.password)
+            const resolve: any = await requestPost('login', payload)
+            if (resolve.token) {
+                bindJWTToken(resolve.token)
+                message.success('登入成功')
+                reducer(ELogin.RSetState, { currentUser: resolve, roleInfo: resolve.roleInfo, status: ELocalStorage.Autherized })
+            }
+        },
+        async [ELogin.EPosLogin]({ state, payload }: any, { reducer, select, effect }: any) {
+            const oldToken = localStorage.getItem(ELocalStorage.Token) //拿到浏览器的token，如果有就走token换token，否则就走登入界面
+            if (oldToken) {
+                const response = await requestPost('token', { token: oldToken })
+                console.log('response: ', response);
+                const { userInfo, token } = response
+                if (token) {
+                    bindJWTToken(token)
+                    localStorage.setItem(ELocalStorage.Token, token)
+                    reducer(ELogin.RSetState, { currentUser: userInfo, status: ELocalStorage.Autherized })
+                } else {
+                    reducer(ELogin.RSetState, { status: ELocalStorage.Login })
+                }
+            } else {
+                reducer(ELogin.RSetState, { status: ELocalStorage.Login })
+            }
+        },
     }
 }
