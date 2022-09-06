f8e2c4743ab43921d0248dbb4a13c38117bfc0e8
yanjike <2711396853@qq.com>
2022-09-06 19:26:27 +0800

    路由权限开发中

--- viteAdminTs/src/pages/App.tsx
+++ viteAdminTs/src/pages/App.tsx
@@ -1,58 +1,58 @@
 import { BasicLayoutProps, PageContainer, ProLayout } from '@ant-design/pro-components'
-import { useState } from 'react'
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import HeadPortrait from '../components/HeadPortrait'
 import defaultRouter from './router'
 import vite from '../../public/vite.svg'
 import './app.less'
+import accessRouter from './access'
+import { effect, useConnect } from '../utils'
+import { ELogin } from '../models/Login'
+import Login from './Login'
+import { ELocalStorage } from '../cofing/Enum'
 
 export default () => {
-  const { pathname } = useLocation();
-  const nav = useNavigate();
-  const [settings, setSetting] = useState<BasicLayoutProps>({
-    contentWidth: "Fluid",
-    fixSiderbar: true,
-    headerHeight: 48,
-    layout: "mix",
-    navTheme: "light",
-    primaryColor: "#1890ff",
-    splitMenus: false,
-  });
-  return <div>
-    <ProLayout
-      title="Vite"
-      style={{ height: "100vh" }}
-      {...settings}
-      {...defaultRouter}
-      location={{ pathname }}
-      logo={<img src={vite} />}
-      rightContentRender={() => <HeadPortrait />}
-      menuItemRender={(item, dom) => (
-        <a onClick={() => nav(item.itemPath)}>{dom}</a>
-      )}
-      itemRender={(route) => (
-        <a>{route.breadcrumbName}</a>
-      )}
-    >
-      <PageContainer title={false}>
-        <Routes location={pathname}>
-          {defaultRouter.route.routes.map(
-            ({ name, path, component, routes }, i: number) => {
-              return (
-                <Route key={i} path={path} element={component}>
-                  {routes &&
-                    routes.map(({ path: path2, component }, j) => {
-                      let subPath = path2.slice(path.length + 1);
-                      return (
-                        <Route key={j} path={subPath} element={component} />
-                      );
-                    })}
-                </Route>
-              );
-            }
-          )}
-        </Routes>
-      </PageContainer>
-    </ProLayout>
-  </div>
+	const { currentUser, status, roleInfo } = useConnect(ELogin.Name)
+	console.log('roleInfo: ', roleInfo);
+	console.log('status: ', status);
+	const { pathname } = useLocation();
+	const nav = useNavigate();
+	const [settings, setSetting] = useState<BasicLayoutProps>({
+		contentWidth: "Fluid",
+		fixSiderbar: true,
+		headerHeight: 48,
+		layout: "mix",
+		navTheme: "light",
+		primaryColor: "#1890ff",
+		splitMenus: false,
+	});
+	useEffect(() => {
+		effect(ELogin.Name, ELogin.EPosLogin)
+	}, [])
+	if (status == ELocalStorage.Login) {
+		return <Login />
+	} else {
+		return <div>
+			<ProLayout
+				title="Vite"
+				style={{ height: "100vh" }}
+				{...settings}
+				{...defaultRouter}
+				location={{ pathname }}
+				logo={<img src={vite} />}
+				rightContentRender={() => <HeadPortrait />}
+				menuItemRender={(item, dom) => (
+					<a onClick={() => nav(item.itemPath)}>{dom}</a>
+				)}
+				itemRender={(route) => (
+					<a>{route.breadcrumbName}</a>
+				)}
+			>
+				<PageContainer title={false}>
+					{/* {accessRouter(0)} */}
+				</PageContainer>
+			</ProLayout>
+		</div>
+	}
+
 }
--- viteAdminTs/src/pages/Login/index.tsx
+++ viteAdminTs/src/pages/Login/index.tsx
@@ -1,3 +1,12 @@
+import { Button } from "antd"
+import { ELogin } from "../../models/Login"
+import { effect } from "../../utils"
+import { useNavigate } from 'react-router-dom'
 export default () => {
-    return <div>登入</div>
+    const nav = useNavigate();
+    return <Button onClick={() => {
+        effect(ELogin.Name, ELogin.EPost, { account: '16666666666', password: '123123' }).then(() => {
+            nav('/')
+        })
+    }}>登入</Button>
 }
