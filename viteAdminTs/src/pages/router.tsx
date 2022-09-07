d6c070293eee6b2c3bceff50d0233a0d02b6c9e0
yanjike <2711396853@qq.com>
2022-09-07 14:55:34 +0800

    增加类似key一样的东西，在最后好做判断并且屏蔽路由

--- viteAdminTs/src/pages/router.tsx
+++ viteAdminTs/src/pages/router.tsx
@@ -2,28 +2,33 @@ import { SmileOutlined } from "@ant-design/icons";
 import Account from "./Admin/Account";
 import Role from "./Admin/Role";
 import Home from "./Home";
+import { EAdminAccess } from "../cofing/Enum";
 export default {
     route: {
         path: "/",
         routes: [
-              {
+            {
                 path: "/",
                 name: "首页",
                 icon: <SmileOutlined />,
+                access: EAdminAccess.ExamineeList,
                 component: <Home />,
-              },
+            },
             {
                 path: "/admin",
                 name: "管理员",
+                access: EAdminAccess.LatentCounselor || EAdminAccess.LatentDelete,
                 routes: [
                     {
                         path: "/admin/account",
                         name: "账号",
+                        access: EAdminAccess.LatentDelete,
                         component: <Account />,
                     },
                     {
                         path: "/admin/role",
                         name: "角色",
+                        access: EAdminAccess.LatentCounselor,
                         component: <Role />,
                     },
                 ],
