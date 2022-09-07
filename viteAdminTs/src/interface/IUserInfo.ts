185b442388c6a9c43b81086026efea478456c854
yanjike <2711396853@qq.com>
2022-09-07 14:50:30 +0800

    角色数据类型

--- viteAdminTs/src/interface/IUserInfo.ts
+++ viteAdminTs/src/interface/IUserInfo.ts
@@ -1,6 +1,14 @@
 export interface IUserInfo {
+    account: string;
+    id: number;
+    name: string
+    password: string
+    remark: string
+    status: boolean
+}
+export interface IRoleInfo {
     name: string; //角色名称
     level: number; //角色级别
-    permission: Array<string>; //角色权限
+    permission: string[]; //角色权限
     status: boolean; //角色状态
 }
